1ce5ecce9843315015255fc882be7478feee789c
ljy4096 <80013886+ljy4096@users.noreply.github.com>
2022-08-29 13:54:44 +0900

    Add files via upload

---
+++ constellationData.js
@@ -0,0 +1,90 @@
+const constellationData = [
+    {
+        "eng":"ursae_minoris",
+        "kor":"작은곰자리"
+    },
+    {
+        "eng":"ursae_majoris",
+        "kor":"큰곰자리"
+    },
+    {
+        "eng":"librae",
+        "kor":"천칭자리"
+    },
+    {
+        "eng":"leonis",
+        "kor":"사자자리"
+    },
+    {
+        "eng":"virginis",
+        "kor":"천칭자리"
+    },
+    {
+        "eng":"eridani",
+        "kor":"에리다누스자리"
+    },
+    {
+        "eng":"ophiuchi",
+        "kor":"땅꾼자리"
+    },
+    {
+        "eng":"geminorum",
+        "kor":"쌍둥이자리"
+    },
+    {
+        "eng":"lyrae",
+        "kor":"거문고자리"
+    },
+    {
+        "eng":"draconis",
+        "kor":"용자리"
+    },
+    {
+        "eng":"cassiopeiae",
+        "kor":"카시오페이아자리"
+    },
+    {
+        "eng": "orionis",
+        "kor":"오리온자리"
+    },
+    {
+        "eng": "cygni",
+        "kor": "백조자리"
+    },
+    {
+        "eng": "tauri",
+        "kor": "황소자리"
+    },
+    {
+        "eng": "cancri",
+        "kor": "게자리"
+    },
+    {
+        "eng": "scorpii",
+        "kor": "전갈자리"
+    },
+    {
+        "eng": "arietis",
+        "kor": "양자리"
+    },
+    {
+        "eng": "aquarii",
+        "kor": "물병자리"
+    },
+    {
+        "eng": "sagittarii",
+        "kor": "궁수자리"
+    },
+    {
+        "eng": "piscium",
+        "kor": "물고기자리"
+    },
+    {
+        "eng": "draconis",
+        "kor": "용자리",
+    },
+    {
+        "eng": "aurigae",
+        "kor": "마차부자리"
+    }
+];
