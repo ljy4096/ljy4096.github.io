1a7f3f2d7d60859e34d314b875a5ba0c8f666c7c
ljy4096 <80013886+ljy4096@users.noreply.github.com>
2022-09-01 23:12:21 +0900

    Add files via upload

---
+++ starData.js
@@ -0,0 +1,70 @@
+const starData = [
+    {
+        "eng": "sirius",
+        "kor": "시리우스"
+    },
+    {
+        "eng": "arcturus",
+        "kor": "아크투루스"
+    },
+    {
+        "eng": "polaris",
+        "kor": "북극성"
+    },
+    {
+        "eng": "rigel",
+        "kor": "리겔"
+    },
+    {
+        "eng": "betelgeuse",
+        "kor": "베텔기우스"
+    },
+    {
+        "eng": "vega",
+        "kor": "베가"
+    },
+    {
+        "eng": "deneb",
+        "kor": "데네브"
+    },
+    {
+        "eng": "altair",
+        "kor": "알타이르"
+    },
+    {
+        "eng": "capella",
+        "kor": "카펠라"
+    },
+    {
+        "eng": "procyon",
+        "kor": "프로키온"
+    },
+    {
+        "eng": "antares",
+        "kor": "안타레스"
+    },
+    {
+        "eng": "spica",
+        "kor": "스피카"
+    },
+    {
+        "eng": "canopus",
+        "kor": "카노푸스"
+    },
+    {
+        "eng": "fomalhaut",
+        "kor": "포말하우트"
+    },
+    {
+        "eng": "pollux",
+        "kor": "폴룩스"
+    },
+    {
+        "eng": "castor",
+        "kor": "카스토르"
+    },
+    {
+        "eng": "regulus",
+        "kor": "레굴루스"
+    }
+]
